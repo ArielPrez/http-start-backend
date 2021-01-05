a179c1de7f3f9685c182f7f8fcab035098a138e2
ArielPrez <arielpgmm@gmail.com>
2021-01-04 22:54:53 -0500

    Adding Query Params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -27,10 +27,14 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
     .get('https://recipestore-2020ap-default-rtdb.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({'Custom-Header':'Hello!!!'})
+          headers: new HttpHeaders({'Custom-Header':'Hello!!!'}),
+          params: searchParams
         }
     ).pipe(
       map(
