27bf300a60b38348cbb4c9b5ce7e22401281a73e
ArielPrez <arielpgmm@gmail.com>
2021-01-14 09:48:40 -0500

    Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LogginInterceptorInterceptor } from './loggin-interceptor.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -9,11 +10,18 @@ import { AuthInterceptorService } from './auth-interceptor-service';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LogginInterceptorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth-interceptor-service.ts
+++ src/app/auth-interceptor-service.ts
@@ -15,21 +15,22 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('Request is on its way... ');
-    console.log(request.url);
+    // console.log('Request is on its way... ');
+    // console.log(request.url);
     const modifiedReq = request.clone({
       headers: request.headers.append('Auth_name', 'xyz_value')
     });
     // To manipulate the response, with an observable (.pipe)
-    return next.handle(modifiedReq).pipe(tap(
-      (event) => {
-        console.log(event);
-        if (event.type === HttpEventType.Response) {
-          console.log('Respose arrived, status & body data: \n' + event.status);
-          console.log(event.body);
-          console.log('\n');
-        }
-      }
-    ));
+    return next.handle(modifiedReq);
+    // .pipe(tap(
+    //   (event) => {
+    //     console.log(event);
+    //     if (event.type === HttpEventType.Response) {
+    //       console.log('Respose arrived, status & body data: \n' + event.status);
+    //       console.log(event.body);
+    //       console.log('\n');
+    //     }
+    //   }
+    // ));
   }
 }
